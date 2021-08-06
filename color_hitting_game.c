b8c6cfb3dc3af9a0b040dd8c7e560cca50c8ed79
kamiokan <spherenov@gmail.com>
2021-08-06 09:57:29 +0900

    「問題を作成する」を作る

--- color_hitting_game.c
+++ color_hitting_game.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main(void) {
+    char q1 = 'R';
+    char q2 = 'G';
+    char q3 = 'B';
+    char q4 = 'Y';
+
     puts("【色当てゲーム】");
     puts("ゲームを始めてください。");
     return EXIT_SUCCESS;
