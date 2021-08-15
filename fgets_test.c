d85220c9bdb0185a786f50c570f1b54d6eff8205
kamiokan <spherenov@gmail.com>
2021-08-15 09:30:14 +0900

    strchr関数を使って入力から改行文字を削る

--- fgets_test.c
+++ fgets_test.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
     char buf[10];
+    char *cr;
     const int size = sizeof(buf);
     fgets(buf, size, stdin);
+    cr = strchr(buf, '\n');
+    if (cr != NULL) {
+        *cr = '\0';
+    }
     puts(buf);
     fgets(buf, size, stdin);
+    cr = strchr(buf, '\n');
+    if (cr != NULL) {
+        *cr = '\0';
+    }
     puts(buf);
     return EXIT_SUCCESS;
 }
